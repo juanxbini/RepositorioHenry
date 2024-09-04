9baa12504318849404ec2fa4e6ac83b7afde016d
unknown <juanxbini@gmail.com>
2024-09-04 18:26:08 -0300

    funciones 5JS

---
+++ JavaScript/Funciones.js
@@ -0,0 +1,16 @@
+// Formas de declarar una función 
+
+// PRIMERO
+function sumaTres(x) {
+    return x + 3;
+}
+
+// SEGUNDO 
+var sumaTres = function (x) {
+    return x + 3; 
+}
+
+//TERCERO: Funcion Flecha
+var sumaTres = (x) => {
+    return x + 3
+}
---
+++ JavaScript/Variables.js
@@ -0,0 +1,30 @@
+var cajon;
+
+//string
+
+var bootcamp = 'Henry';
+
+// Numeros 
+
+var numeroEntero = 17;
+var numeroNegativo = -4;
+var numeroDecimal = 11.75;
+
+//Booleanos
+
+var estoyEntendiendo = true;
+var estoyAburrido = false;
+
+// Undefined
+
+var cajon1;
+console.log(cajon1);
+
+// Null
+
+var sinNada = null; 
+
+// Método length
+
+var valorMetodoLength = bootcamp.length
+console.log(valorMetodoLength)
